852dc242dbfaccd072a3d24853bcca8f35b7a9cc
Eduarda Marangão <eduardaamorimmarangao@gmail.com>
2024-07-17 17:23:16 -0400

    aula14

---
+++ cursoJS/aula14/aula11/ex.009.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+console.log(`Vivendo em ${pais}.`)
+if (pais == 'Brasil'){ // pais != Brasil
+    console.log('Você é Brasileiro!') // estrangeiro
+} else {
+    console.log('Você é Estrageiro!') // brasileiro
+}
